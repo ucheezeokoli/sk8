55def9e05704c387ceffe4087cfa340ac0a8f88e
ucheezeokoli <ucheezeokoli@gmail.com>
2019-09-12 15:12:58 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Weekly Skate Session
+        </p>
+        <p>hit button recieve notification</p>
+        <Button variant="contained" color="secondary">
+          :)
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
+   
---
+++ src/button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+<div>
+    <Button variant="primary" size="lg">
+        :)
+    </Button>
+</div>
+    
