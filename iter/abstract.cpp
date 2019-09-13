9542667c178781aa5f03c6f24debb5fbb5aa771d
Hoa <hnguyen257@ucmerced.edu>
2019-09-12 18:54:20 -0700

    isSkating()

--- iter/abstract.cpp
+++ iter/abstract.cpp
@@ -7,8 +7,17 @@ Think of an idea, and realize it in code here
 
 */
 
-
-
+bool nowSkating(int* skater_profile)
+{
+    //check if the user is skating, whether by environment or manual input 
+    if(skater_profile != skating && isSkating()){
+        //update status of skater
+        //do other things
+        return true;
+    }
+    
+    return false;
+}
 
 int main(){
 return 0;
