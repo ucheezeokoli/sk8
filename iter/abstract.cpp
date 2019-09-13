3a76053dce63a2a5f068126e2f6a493d4f369af4
Hoa <hnguyen257@ucmerced.edu>
2019-09-12 18:44:38 -0700

    created dir, added file for notes

---
+++ iter/abstract.cpp
@@ -0,0 +1,19 @@
+/*
+This file acts as a document to outline the skeleton of the project.
+A high level representation of key features and functions.
+
+Not a real program, code however you like
+Think of an idea, and realize it in code here
+
+*/
+
+
+
+
+int main(){
+return 0;
+}
+
+
+
+
